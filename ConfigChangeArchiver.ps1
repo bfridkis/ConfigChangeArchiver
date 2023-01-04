cd58f84137a4b643d3271bdd26fbdca9323a8886
Benjamin Fridkis <32713413+bfridkis@users.noreply.github.com>
2023-01-04 14:41:18 -0600

    Add files via upload

---
+++ ConfigChangeArchiver.ps1
@@ -0,0 +1,7 @@
+﻿$Todays = Get-ChildItem -Path "C:\Program Files (x86)\SolarWinds\Orion\NCM\Jobs\Daily Config Change Report.htm"
+$NewestArchieved = Get-ChildItem -Path "C:\Program Files (x86)\SolarWinds\Orion\NCM\Jobs\Daily Config Change Report Archive" |
+                   Where-Object { $_.Extension -eq ".htm" } | Sort-Object { $_.CreationTime } -Descending | Select-Object -First 1
+If ($Todays.LastWriteTime -gt $NewestArchieved.LastWriteTime) { 
+    Copy-Item $Todays.FullName -Destination "C:\Program Files (x86)\SolarWinds\Orion\NCM\Jobs\Daily Config Change Report Archive\"
+    Rename-Item "C:\Program Files (x86)\SolarWinds\Orion\NCM\Jobs\Daily Config Change Report Archive\Daily Config Change Report.htm" -NewName "Daily Config Change Report-$(Get-Date -Format MMddyyyy_HHmmss).htm"            
+}
